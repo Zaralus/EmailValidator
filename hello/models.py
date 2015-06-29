447994e948f6a3cc932c4db4b4dfb0ec7d7d14a3
Zaralus <stephenbrown84@gmail.com>
2015-06-29 21:09:57 -0230

    Adding proxy view to use to call QuickEmailVerification API.

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    fn = models.CharField(max_length=100)
+    fi = models.CharField(max_length=1)
+    mn = models.CharField(max_length=100)
+    mi = models.CharField(max_length=1)
+    ln = models.CharField(max_length=100)
+    li = models.CharField(max_length=1)
+
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 from .models import Greeting
+import urllib2
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
+def isValidEmail(request):
+    email = request.GET.get('email');
+    apiKey = request.GET.get('apiKey');
+
+    url = "http://api.quickemailverification.com/v1/verify?email=" + email + "&apikey=" + self.apiKey
+    jsonData = json.load(urllib2.urlopen( url ))
+    return JsonResponse(jsonData)
 
 def db(request):
 
