4660f385a65071f58a04fc6ff7ac05261dbbff21
Zaralus <stephenbrown84@gmail.com>
2015-07-01 18:19:47 -0230

    Fixed small syntax error in JS.

--- hello/static/EmailValidator.js
+++ hello/static/EmailValidator.js
@@ -177,7 +177,7 @@ function processPerson( rawPerson ) {
 	}
 	else {
 		// No valid email found so write in person with 'N/A'
-		outputData.push(person.fn, person.ln, rawPerson.domain, 'N/A']);
+		outputData.push([person.fn, person.ln, rawPerson.domain, 'N/A']);
 	}
 	
 	return outputData;
