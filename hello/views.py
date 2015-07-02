f10be7fd824f03d764365c48de3aba5664fe815b
Zaralus <stephenbrown84@gmail.com>
2015-07-02 13:28:50 -0230

    Fixing up error handling code.

--- hello/views.py
+++ hello/views.py
@@ -59,7 +59,7 @@ def isvalidemail(request):
     try:
         jsonData = json.load(urllib2.urlopen( url ))
     except urllib2.URLError as e:
-        jsonData = json.load(e.read())
+        jsonData = {"success":"false","message":e.reason}
     return JsonResponse(jsonData)
 
 def db(request):
